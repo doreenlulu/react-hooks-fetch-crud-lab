7dd539f0f4e916057a5039be768527ca30d5c1ac
Doreen <doreenlulu97@gmail.com>
2022-10-10 14:02:13 +0300

    Completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,30 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
-  }
+    const newQuestion = {
+			prompt: formData.prompt,
+			answers: [
+				formData.answer1,
+				formData.answer2,
+				formData.answer3,
+				formData.answer4,
+			],
+			correctIndex: parseInt(formData.correctIndex),
+		};
+		fetch("http://localhost:4000/questions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newQuestion),
+		}).then((r) => {
+			if (r.ok) {
+				r.json().then((newQuestion) => {
+					props.handleAddQuestion(newQuestion);
+				});
+			}
+		});
+	}
 
   return (
     <section>
@@ -88,6 +110,6 @@ function QuestionForm(props) {
       </form>
     </section>
   );
-}
+  }
 
 export default QuestionForm;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,21 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
+function QuestionList({ questions, handeDelete }) {
+	return (
+		<section>
+			<h1>Quiz Questions</h1>
+			<ul>
+				{questions.map((question) => (
+					<QuestionItem
+						key={question.id}
+						question={question}
+						handleDelete={handeDelete}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 }
 
 export default QuestionList;
